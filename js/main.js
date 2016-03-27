3a01b2b3f7bc9334a3149034788b032f32b9bfdc
ca1tlin22 <gagn0372@algonquinlive.com>
2016-03-27 19:32:25 -0400

    ball dimensions altered, div changed to .ball to satisfy market

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 var $html = $('html');
-var $ball = $('div');
+var $ball = $('.ball');
 var $picker = $('form');
 var $input = $('#pick-color');
 
-$picker.on('change', function() {
+$picker.on('change', function () {
   console.log('works');
   $ball.css('background-color', $input.val());
 });
